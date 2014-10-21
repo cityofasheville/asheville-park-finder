4a0735287e2f6b3449786384bb5f452ad4ac0843
carlyleec <carlyleec@gmail.com>
2014-10-21 15:49:33 -0400

    first gh-pages

---
+++ README.md
@@ -0,0 +1,4 @@
+asheville-park-finder
+=====================
+
+Find City of Asheville Parks to meet your needs
---
+++ js/park-options.js
@@ -0,0 +1,17 @@
+var parkOptions = [
+	{value : 'all', label : 'Choose an option', icon : 'none'},
+    {value : 'Baseball_F', label : 'Baseball/Softball Fields', icon : 'baseball.png'},
+    {value : 'Basketball', label : 'Basketball Courts', icon : 'basketballfield.png'},
+    {value : 'Cycling', label : 'Cycling Paths/Velodrome', icon : 'cycling.png'},
+    {value : 'Dog_Park', label : 'Dog Parks', icon : 'dogs_leash.png'},
+    {value : 'Grills', label : 'Grills', icon : 'grill.png'},
+    {value : 'Field', label : 'Multi-use Sports Fields', icon : 'soccer.png'},
+    {value : 'Picnic_She', label : 'Picnic Shelters', icon : 'picnicShelter.png'},
+    {value : 'Picnic_Tab', label : 'Picnic Tables', icon : 'picnicTable.png'},
+    {value : 'Playrgound', label : 'Playgrounds', icon : 'playground.png'},
+    {value : 'Restrooms', label : 'Restrooms', icon : 'toilets.png'},
+    {value : 'Recreation', label : 'Recreation Center', icon : 'communitycentre.png'},
+    {value : 'Tennis_Cou', label : 'Tennis Court', icon : 'tennis.png'},
+    {value : 'Multi_Use_', label : 'Walking Paths', icon : 'walkingtour.png'},
+    {value : 'WI_FI', label : 'Wi-Fi', icon : 'wi-fi.png'}   
+];
